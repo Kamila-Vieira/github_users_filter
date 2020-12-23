1b9f78ddb1f4d86a0f8cdd3a877d8ba88a0f4ac8
Kamila Almeida <kamila.almeida@acertonline.local>
2020-12-23 09:00:18 -0300

    Mostrando todos os usuários/estruturação de filtros

--- js/index.js
+++ js/index.js
@@ -1,61 +1,54 @@
 "use strict";
 
 window.addEventListener("load", async () => {
-  await fetchGithubUser('Kamila-Vieira');
+  await fetchAllGithubUsers();
+  window.addEventListener("input", await searchGithubUser);
 });
 
+
+let githubUsers = [];
 const listUserContainer = document.querySelector('.grid-user-container');
 const inputSearchUser = document.querySelector('.input-search-user');
 
-window.addEventListener("input", async () => {
-  await fetchGithubUser(inputSearchUser.value);
-});
+const fetchAllGithubUsers = async () =>{
+  const URL = `https://api.github.com/users`
+  const response = await fetch(URL);
+  const json = await response.json().then(async data => await data);
+  githubUsers = json.map(user => {
+    const { login } = user;
+    return fetchGithubUserData(login);
+  })
+  return githubUsers;
+}
 
-let githubUsers = [];
-/* function searchGithubUser(user){
-  return fetchGithubUser(user)
-} */
-
-const fetchGithubUser = async (user) =>{
+const fetchGithubUserData = async (user) =>{
   const response = await fetch(`https://api.github.com/users/${user}`);
   const json = await response.json().then(async data => await data);
-  const { name, avatar_url, created_at, location} = json;
-  return createUserCard(name, avatar_url, created_at, location)
+  const { name, avatar_url, created_at, location, bio} = json;
+  return createUserCard(name, avatar_url, created_at, location, bio)
 }
 
-function createUserCard(name, picture, created, location){
+function createUserCard(name, picture, created, location, bio){
   const userCard = document.createElement('li');
-  const userName = document.createElement('p');
-  const userPicture = document.createElement('img');
-  userPicture.setAttribute('src', picture)
-  const userCreated = document.createElement('p');
-  const userLocation = document.createElement('p');
-  userCard.appendChild(userName);
-  userCard.appendChild(userPicture);
-  userCard.appendChild(userCreated);
-  userCard.appendChild(userCreated);
+  const newUser = {
+    picture: document.createElement('img'),
+    name: document.createElement('p'),
+    creationDate: document.createElement('p'),
+    location: document.createElement('p'),
+    bio: document.createElement('p')
+  }
+  for (const data in newUser) {
+    userCard.appendChild(newUser[data])
+  }
+  newUser.picture.setAttribute('src', picture)
+  newUser.name.textContent = name;
+  newUser.creationDate.textContent = created;
+  newUser.location.textContent = location;
+  newUser.bio.textContent = bio;
   listUserContainer.appendChild(userCard);
-  userName.textContent = name;
-  userCreated.textContent = created;
-  userLocation.textContent = location;
 }
 
+const searchGithubUser = async () => {
+  await fetchGithubUserData(inputSearchUser.value);
+}
 
-
-/* 
-const fetchDevs = async () => {
-  const res = await fetch("http://localhost:3001/devs");
-  const json = await res.json();
-  allDevs = json.map((dev) => {
-    const { id, name, picture, programmingLanguages } = dev;
-    const nameLowerCase = name.toLowerCase();
-    const nameToSearch = removeAccentsAndSpaces(nameLowerCase);
-    return {
-      id,
-      name,
-      nameToSearch,
-      picture,
-      programmingLanguages,
-    };
-  });
-} */
