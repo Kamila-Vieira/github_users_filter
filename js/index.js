b9336a938b50c68742ab0efc86a29fe0da1f8615
Kamila Almeida <kamila.almeida@acertonline.local>
2020-12-22 21:07:22 -0300

    criação de card de usuário

--- js/index.js
+++ js/index.js
@@ -1,13 +1,61 @@
-var div = document.querySelector('.grid');
-var xhr = new XMLHttpRequest();
+"use strict";
 
-var data = new Object();
-async function fetchAllGithubUsers(user){
-  var URL = `https://api.github.com/users/${user}`
-  return await fetch(URL)
-  .then(async response => await response.json()) 
-  .then(json => json)
-  .catch(err => console.log(err));
+window.addEventListener("load", async () => {
+  await fetchGithubUser('Kamila-Vieira');
+});
+
+const listUserContainer = document.querySelector('.grid-user-container');
+const inputSearchUser = document.querySelector('.input-search-user');
+
+window.addEventListener("input", async () => {
+  await fetchGithubUser(inputSearchUser.value);
+});
+
+let githubUsers = [];
+/* function searchGithubUser(user){
+  return fetchGithubUser(user)
+} */
+
+const fetchGithubUser = async (user) =>{
+  const response = await fetch(`https://api.github.com/users/${user}`);
+  const json = await response.json().then(async data => await data);
+  const { name, avatar_url, created_at, location} = json;
+  return createUserCard(name, avatar_url, created_at, location)
 }
-console.log(fetchAllGithubUsers('Kamila-Vieira').login)
 
+function createUserCard(name, picture, created, location){
+  const userCard = document.createElement('li');
+  const userName = document.createElement('p');
+  const userPicture = document.createElement('img');
+  userPicture.setAttribute('src', picture)
+  const userCreated = document.createElement('p');
+  const userLocation = document.createElement('p');
+  userCard.appendChild(userName);
+  userCard.appendChild(userPicture);
+  userCard.appendChild(userCreated);
+  userCard.appendChild(userCreated);
+  listUserContainer.appendChild(userCard);
+  userName.textContent = name;
+  userCreated.textContent = created;
+  userLocation.textContent = location;
+}
+
+
+
+/* 
+const fetchDevs = async () => {
+  const res = await fetch("http://localhost:3001/devs");
+  const json = await res.json();
+  allDevs = json.map((dev) => {
+    const { id, name, picture, programmingLanguages } = dev;
+    const nameLowerCase = name.toLowerCase();
+    const nameToSearch = removeAccentsAndSpaces(nameLowerCase);
+    return {
+      id,
+      name,
+      nameToSearch,
+      picture,
+      programmingLanguages,
+    };
+  });
+} */
